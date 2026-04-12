4510ca8b2f4c5d96d296e76e432d73d30dae33a8
秦 <197587650+liver-m@users.noreply.github.com>
2026-04-12 15:33:21 +0800

    feat:add unified error response with ErrorResponse DTO

--- src/main/java/com/zjut/student/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zjut/student/exception/GlobalExceptionHandler.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(StudentNotFoundException.class)
-    public ResponseEntity<String> HandlerStudentNotFound(Exception ex){
-        return ResponseEntity.status(404).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handlerStudentNotFound(StudentNotFoundException ex){
+        ErrorResponse errorResponse = new ErrorResponse(404,ex.getMessage(),"2026-04-12");
+        return ResponseEntity.status(404).body(errorResponse);
     }
 }
